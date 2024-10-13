d1627475f4119729e1f53b7bda1337f814352e2a
Satvik Mathur <52615238+SStealthyDevelops@users.noreply.github.com>
2024-10-13 16:21:19 -0400

    Added O() notation to methods in ArrayList

--- src/dsa/ArrayList.java
+++ src/dsa/ArrayList.java
@@ -22,10 +22,13 @@ public class ArrayList<T>{
         this.size = 0;
     }
 
+    // O(1)
     public T get(final int index) {
         return this.array[index];
     }
 
+    // O(1) if size can sustain
+    // O(n) worst-case
     public ArrayList<T> addItem(T item) {
         if (size == array.length) {
             extendArray();
@@ -35,6 +38,7 @@ public class ArrayList<T>{
         return this;
     }
 
+    // O(n)
     private void extendArray(int newSize) {
         T[] tempArray = (T[]) new Object[newSize];
         for (int i = 0; i < size; i++) {
@@ -46,6 +50,7 @@ public class ArrayList<T>{
         extendArray(multiplicity * array.length);
     }
 
+    // O(n)
     public ArrayList<T> removeItem(final int index) {
         array[index] = null;
         shiftLeft(index);
@@ -53,6 +58,8 @@ public class ArrayList<T>{
         return this;
     }
 
+    // O(1) if index within current array size
+    // O(n) if not (worst-case)
     public ArrayList<T> setItem(final int index, final T item) {
         if (index >= array.length) {
             extendArray(index * multiplicity);
@@ -61,6 +68,7 @@ public class ArrayList<T>{
         return this;
     }
 
+    // O(n)
     private ArrayList<T> shiftLeft(final int startingIndex) {
         for (int i = startingIndex; i < size; i++) {
             array[i] = array[i + 1];
@@ -69,6 +77,7 @@ public class ArrayList<T>{
         return this;
     }
 
+    // O(n)
     public T[] toArray() {
         T[] tempArray = (T[]) new Object[size];
         System.arraycopy(array, 0, tempArray, 0, size);
